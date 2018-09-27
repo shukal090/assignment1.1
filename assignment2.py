a1315981960741e08a1cfd103a0a40fd113c58f6
shukal090 <shukal.jain090@gmail.com>
2018-09-27 23:22:39 +0530

    first commit

---
+++ assignment2.py
@@ -0,0 +1,46 @@
+1
+print('hello')
+hello
+
+2
+print('acad',"+",'view')
+	  
+acad + view
+
+3
+ print('x is 10','y is 20','z is 30')
+	  
+x is 10 y is 20 z is 30
+
+4
+>>> print("lets get started")
+	  
+lets get started
+
+5
+send=('%s="%s"')
+	  
+>>> send%("s","Acadview")
+	  
+s="Acadview"
+
+send=('%s="%s"')
+	  
+>>> send%('course',"python")
+	  
+course="python"
+
+>>> send=('%s=%s')
+6
+print("enter the radius of circle")
+radius=int(input())
+print()
+ans=3.14*(radius*radius)
+print(ans)
+
+	  
+>>> send%('fess','5000')
+	  
+fess=5000
+
+6
