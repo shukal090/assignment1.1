8574bc41a62039888742029c557e50b650a448d7
shukal090 <43571538+shukal090@users.noreply.github.com>
2018-10-16 09:30:00 +0530

    Update assign_class_object1.py

--- assign_class_object1.py
+++ assign_class_object1.py
@@ -1,3 +1,17 @@
+#Q.1- Create a user defined dictionary and get keys corresponding to the value using for loop. 
 d={'messi':10,'ronaldo':7,'becham':9}
 for number_key,values in d.items():
     print(number_key,"corresponds to",d[number_key])
+#Q.2- Create a dictionary and store student names and create nested dictionary to store the subject wise marks of every student.Print the marks of a given student from that dictionary for every subject.
+student = {
+            "Ram":{"Maths":85,"Science":80,"English":83},
+            "Sham":{"Maths":59,"Science":70,"English":75}
+          }
+studentName = input("Enter the name of student: ")
+if studentName in student:
+    print(studentName)
+    for key,value in student[studentName].items():
+        print(key,value)
+else:
+    print("Student not found")
+
